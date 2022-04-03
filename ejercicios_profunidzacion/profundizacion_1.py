8b852a8ec69f1b8590f900dbe3eba48503de8cdc
nehuenignaciog <nehuenignaciog@gmail.com>
2022-04-03 18:44:08 -0300

    Pendiente de revisión

--- ejercicios_profunidzacion/profundizacion_1.py
+++ ejercicios_profunidzacion/profundizacion_1.py
@@ -4,7 +4,7 @@
 # Autor: Inove Coding School
 # Version: 2.0
 
-# NOTA: 
+# NOTA:
 # Estos ejercicios son de mayor dificultad que los de clase y práctica.
 # Están pensados para aquellos con conocimientos previo o que dispongan
 # de mucho más tiempo para abordar estos temas por su cuenta.
@@ -30,12 +30,29 @@ print('Comenzamos a ponernos serios!')
 # inicio = ....
 # fin = ....
 
+inicio = int(input("Ingrese el primer número de la secuencia\n"))
+
+fin = int(input("Ingrese el último número de la secuencia\n"))
+
 # cantidad_numeros ....
 # sumatoria ....
 
+cantidad_numeros = 0
+
+sumatoria = 0
+
 # bucle.....
+
+for i in range(inicio, fin+1, +1):
+    sumatoria += i
+    cantidad_numeros += 1
 
 # Al terminar el bucle calcular el promedio como:
 # promedio = sumatoria / cantidad_numeros
 
+
+promedio = sumatoria / cantidad_numeros
+
 # Imprimir resultado en pantalla
+
+print ("Promedio: ", promedio)
