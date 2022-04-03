bf5a0c994b0ec7243aef7da9cce5c0bdc39a1f84
nehuenignaciog <nehuenignaciog@gmail.com>
2022-04-02 23:12:34 -0300

    Pendiente de revisión

--- ejercicios_practica/ejercicio_2.py
+++ ejercicios_practica/ejercicio_2.py
@@ -11,15 +11,26 @@
 
 # Dado la siguiente lista de colores, utilizar "for"
 # para imprimir en pantalla todos los colores
+from tkinter import colorchooser
+
+
 colores = ['rojo', 'naranja', 'verde', 'azul']
 
 # Itere el "for" utilizando la lista como parámero
 # y utilizar como elemento del "for" cada color
 # for color ...
 
+
+for color in colores:
+    print ("Color: ", color)
 # Itere el "for" utilizando el tamaño de la lista
 # como parámetro y utilizar el índice para acceder a
 # los elementos de la lista
 # for i ...
+
+len_colores = len(colores)
+
+for i in range(len_colores):
+    print ("Color: ", colores[i])
 
 print("terminamos!")
